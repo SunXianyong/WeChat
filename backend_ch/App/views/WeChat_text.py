229ed08c24a775ab2a1f964e4833a0fb8f467e9b
sunxy <sunxy@aithu.com>
2019-02-13 18:45:57 +0800

    .

--- backend_ch/App/views/WeChat_text.py
+++ backend_ch/App/views/WeChat_text.py
@@ -5,10 +5,11 @@ import urllib3
 
 
 def get_text(url):
+    url ="http://mp.weixin.qq.com/s?__biz=MjM5MDEyMDk4Mw==&amp;mid=2650168151&amp;idx=1&amp;sn=e6848e64f140d375f7d007359032fec8&amp;chksm=be4b562f893cdf394b04f71167ed89c1c3a95c37a829adf13a11ed216b1b3a9923c074bec1bb&amp;scene=0&amp;xtrack=1#rd"
     http = urllib3.PoolManager()
     html = http.request('GET',url)
 
-    soup = BeautifulSoup(html.data,features="lxml-xml")
+    soup = BeautifulSoup(html.data,features="lxml")
     all_content = soup.find('div',id='img-content')
     main_content = all_content.find("div",id="js_content")
 
@@ -23,3 +24,4 @@ def get_text(url):
 
     return "5"
 
+get_text("url")
