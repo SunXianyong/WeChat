94d25496c3eafd7e4c54fab698c6e5ef0b891355
SunXianYong <sxy122333@gmail.com>
2019-02-14 12:36:57 +0800

    .

--- backend_ch/App/utils/wechat_upload.py
+++ backend_ch/App/utils/wechat_upload.py
@@ -6,10 +6,12 @@ import urllib3
 
 # 获取access_token
 http = urllib3.PoolManager()
-url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx22567ecd41dcc21b&secret=wx22567ecd41dcc21b'
+url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx22567ecd41dcc21b&secret=a886e801f66de74eeefe44cd73e09840'
 r = http.request('GET', url)
-print(r.data)
+dic = json.loads(r.data.decode('utf-8'))
+access = dic.get
 input()
+r.read
 # 上传缩略图
 filname = "mbry.JPG"
 cmd = f'curl -F media=@{filname} "https://api.weixin.qq.com/cgi-bin/material/add_material?access_token=access_token&type=image"'
