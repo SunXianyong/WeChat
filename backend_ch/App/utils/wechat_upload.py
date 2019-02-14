a6c1e58e33a8b5eede2c4210922439591dd9f9d7
sunxy <sunxy@aithu.com>
2019-02-14 12:36:03 +0800

    上传图片

--- backend_ch/App/utils/wechat_upload.py
+++ backend_ch/App/utils/wechat_upload.py
@@ -1,17 +1,32 @@
 # coding=utf-8
 
-import os
 import urllib3
+import json
 
 
 # 获取access_token
-http = urllib3.PoolManager()
-url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx22567ecd41dcc21b&secret=wx22567ecd41dcc21b'
-r = http.request('GET', url)
-print(r.data)
-input()
+def get_acc():
+    http = urllib3.PoolManager()
+    url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx22567ecd41dcc21b&secret=a886e801f66de74eeefe44cd73e09840'
+    r = http.request('GET', url)
+    dic = json.loads(r.data.decode('utf-8'))
+    access = dic.get("access_token")
+    if not access:
+        raise RuntimeError("获取access 失败")
+
+    return access
+
+
 # 上传缩略图
-filname = "mbry.JPG"
-cmd = f'curl -F media=@{filname} "https://api.weixin.qq.com/cgi-bin/material/add_material?access_token=access_token&type=image"'
-output = os.popen(cmd)
-print(output.read())
+def up_thumb():
+    http = urllib3.PoolManager()
+    access = get_acc()
+    url = f"https://api.weixin.qq.com/cgi-bin/material/add_material?access_token={access}&type=thumb"
+    filname = "mbry.JPG"
+    with open(filname, "rb") as f:
+        file_data = f.read()
+    r = http.request('POST', url, fields={'filefield': (filname, file_data),})
+    dic = json.loads(r.data.decode('utf-8'))
+    print(dic)
+
+up_thumb()
