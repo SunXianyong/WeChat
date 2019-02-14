499eb7f439c560f82521d902df38d6920d71e6b8
sunxy <sunxy@aithu.com>
2019-02-14 16:55:12 +0800

    .

--- backend_ch/App/views/weixin.py
+++ backend_ch/App/views/weixin.py
@@ -68,18 +68,6 @@ def link_type(soup):
     music.append(thumbid)
     soup.MsgType.insert_after(music)
     soup.Url.extract()
-    my_xml = "<xml>" \
-             "<ToUserName>oH-qc1R3KtLpsIEoyIBwgiAM07C4</ToUserName>" \
-             "<FromUserName>gh_c704f0a5b415</FromUserName>" \
-             "<CreateTime>1550121899</CreateTime>" \
-             "<MsgType>music</MsgType>" \
-             "<Music>" \
-             "<Title>为什么天津人都不推荐去狗不理？</Title>" \
-             "<Description>为什么天津人都不推荐去狗不理？</Description>" \
-             "<MusicUrl>https://music.163.com/#/song?id=516728102</MusicUrl>" \
-             "<ThumbMediaId>WaPOTn8FGx9Xug29nk9U0uLAn7Hq424Zmf44v5qi9B0</ThumbMediaId>" \
-             "</Music>" \
-             "</xml>"
     return soup
 
 
