d9a38865bb0c996a7fd4ff1d2a874252f1147672
sunxy <sunxy@aithu.com>
2019-02-14 18:27:57 +0800

    音频提速

--- backend_ch/App/views/textToMp3.py
+++ backend_ch/App/views/textToMp3.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 from aip import AipSpeech
+import threading
 
 """ 你的 APPID AK SK """
 APP_ID = '15508666'
@@ -18,22 +19,32 @@ def get_bin(text,user):
 
     # 识别正确返回语音二进制 错误则返回dict 参照下面错误码
     if not isinstance(result, dict):
-        with open(f'{user}.mp3', 'ab') as f:
-            f.write(result)
+        return result
+        # with open(f'{user}.mp3', 'ab') as f:
+        #     f.write(result)
+    else:
+        raise RuntimeError("转音频错误")
 
 
 # 文本读取
 def make_mp3(user):
+    mp3_lis = []
     with open('txt','r',encoding='utf8') as f:
         lenth = 0
         text = ''
         for i in f:
             i_len = len(bytes(i, encoding='utf-8'))
             if lenth + i_len >= 1023:
-                get_bin(text,user)
+                t = threading.Thread(target=get_bin, args=(text,user))
+                mp3_lis.append(t.start())
                 text = ''
                 lenth = 0
             lenth += i_len
             text += i
-        get_bin(text,user)
+        t = threading.Thread(target=get_bin, args=(text, user))
+        mp3_lis.append(t.start())
+
+    with open(f'{user}.mp3', 'wb') as f:
+        for i in mp3_lis:
+            f.write(i)
     return user
